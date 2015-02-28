78d799b2f1618411f7b504c7c842e9da25157131
mgracer48 <mgracer48@users.noreply.github.com>
2015-02-28 13:04:22 +0100

    Update and rename dependencies-install-cmd-mac .sh to dependencies-install .sh

--- mac/dependencies-install .sh
+++ mac/dependencies-install .sh
@@ -2,4 +2,7 @@ echo " Installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 echo " Installing panda3d dependencies" 
 brew install python apple-gcc42 freetype fftw --with-fortran gtk+ wxmac eigen jpeg libpng ode --enable-double-precision libtiff 
- 
+echo " Installing python stuff" 
+sudo pip install bcrypt pymongo 
+echo " Installing astron stuff"
+brew install mongodb
